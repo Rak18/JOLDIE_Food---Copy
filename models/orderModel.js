58d4dd842307e8d61b007d9672fef5e49b3a292e
Rak18 <raktimranjangogoi954@gmail.com>
2022-07-03 17:48:09 +0530

    adding stripe

---
+++ models/orderModel.js
@@ -0,0 +1,8 @@
+// const mongoose = require('mongoose')
+
+// const orderSchema = mongoose.Schema({
+//     name:{
+//         type:String,
+//         required:[true, 'order name required']
+//     }
+// })
